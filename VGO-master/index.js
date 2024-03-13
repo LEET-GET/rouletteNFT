30fb8a88cccaa3de54754bc9b801ed892b4471a5
LEET-GET <butenko.prosha@gmail.com>
2024-03-13 16:17:56 +0600

    13th

--- VGO-master/index.js
+++ VGO-master/index.js
@@ -1213,12 +1213,12 @@ passport.use(new SteamStrategy({
     done(false, profile);
 }));
 
-app.get('/auth/steam', passport.authenticate('steam'), function (req, res) {
+app.get('/auth/callback', passport.authenticate('steam'), function (req, res) {
     // The request will be redirected to Steam for authentication, so
     // this function will not be called.
 });
 
-app.get('/auth/steam/return', passport.authenticate('steam', { failureRedirect: '/login' }), function (req, res) {
+app.get('/auth/callback', passport.authenticate('steam', { failureRedirect: '/login' }), function (req, res) {
     // Successful authentication, redirect home.
     res.redirect('/');
     console.log("logged in successfully");
