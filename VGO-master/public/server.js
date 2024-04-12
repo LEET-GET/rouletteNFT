0eecac10b51d889732d7398fb56c88d93f8bb584
LEET-GET <butenko.prosha@gmail.com>
2024-04-12 17:01:23 +0600

    40th5

--- VGO-master/public/server.js
+++ VGO-master/public/server.js
@@ -1,37 +1,38 @@
-// Wait for the entire document to load before attaching handlers
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('registrationForm').addEventListener('submit', handleRegistration);
-    document.getElementById('loginBtn').addEventListener('submit', handleLogin);
-    updateNavBasedOnAuth();
-});
 
-function handleRegistration(e) {
+
+// Prevent form submission default action and handle registration
+document.getElementById('registrationForm').addEventListener('submit', function(e) {
     e.preventDefault();
     const email = document.getElementById('registerEmail').value;
     const password = document.getElementById('registerPassword').value;
 
     fetch('http://209.38.248.1:8001/user/register/', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers: {
+            'Content-Type': 'application/json'
+        },
         body: JSON.stringify({ email: email, password: password })
     })
     .then(response => response.json())
     .then(data => {
         console.log('Registration Success:', data);
     })
-    .catch(error => {
+    .catch((error) => {
         console.error('Registration Error:', error);
     });
-}
+});
 
-function handleLogin(e) {
+// Prevent form submission default action and handle login
+document.getElementById('loginForm').addEventListener('submit', function(e) {
     e.preventDefault();
     const username = document.getElementById('loginUsername').value;
     const password = document.getElementById('loginPassword').value;
 
     fetch('http://209.38.248.1:8001/user/login/', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers: {
+            'Content-Type': 'application/json'
+        },
         body: JSON.stringify({ username: username, password: password })
     })
     .then(response => {
@@ -41,29 +42,42 @@ function handleLogin(e) {
     .then(data => {
         localStorage.setItem('accessToken', data.access);
         console.log('Login successful, fetching profile...');
-        window.location.href = 'https://roulettenft.onrender.com/profile.html'; // Redirect after successful login
+        fetchUserProfile(); // Fetch user profile immediately after successful login
+        window.location.href = 'http://127.0.0.1:5500/VGO-master/public/profile.html';
+        fetchUserProfile(); 
     })
-    .catch(error => {
+    .catch((error) => {
         console.error('Login Error:', error);
+        
     });
-}
+});
+fetchUserProfile(); 
 
-function updateNavBasedOnAuth() {
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
     const accessToken = localStorage.getItem('accessToken');
+    updateNavBasedOnAuth(accessToken);
+});
+
+function updateNavBasedOnAuth(accessToken) {
     const signInButton = document.getElementById('sign-button-container');
     const profileButton = document.getElementById('profile-button-container');
     const userBalance = document.getElementById('userBalance');
 
     if (accessToken) {
+        fetchUserProfile(); // Fetch and display user balance
         signInButton.style.display = 'none';
         profileButton.style.display = 'block';
-        fetchUserProfile();
     } else {
         signInButton.style.display = 'block';
         profileButton.style.display = 'none';
-        userBalance.style.display = 'none';
+        if (userBalance) userBalance.style.display = 'none';
     }
 }
+
 
 function fetchUserProfile() {
     const accessToken = localStorage.getItem('accessToken');
@@ -135,5 +149,30 @@ function sendTransaction() {
         alert('Error submitting transaction.');
     });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
